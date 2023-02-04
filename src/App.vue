cbac126ad4b0bcacebe7c72f32fd3168372670e5
Kuznetsov_Leonid <orlan1211@yahoo.com>
2023-02-04 20:11:22 +0300

    этап 3

--- src/App.vue
+++ src/App.vue
@@ -6,95 +6,51 @@
 */
 <template>
     <div class="app">
-        <form @submit.prevent>
-        <h4>Создание поста</h4>
-        <input 
-            v-bind:value="title"
-            @input="title = $event.target.value" 
-            class="input" 
-            type="text" 
-            placeholder="Название"
-            >
-        <input 
-            v-bind:value="body" 
-            @input="body = $event.target.value"
-            class="input" 
-            type="text" 
-            placeholder="Описание"
-            >
-        <button class="btn" @click="createPost">Создать</button>
-        </form>
-        <div class="post" v-for="post in posts">
-            <div><strong>Название:</strong> {{ post.title }}</div>
-            <div><strong>Описание:</strong> {{ post.body }}</div>
-        </div>
+        <post-form
+            @create="createPost"
+        />
+        <post-list 
+            :posts="posts"
+            @remove="removePost"
+        />
     </div>
 </template>
 
 <script>
+    import PostForm from './components/PostForm.vue';
+    import PostList from './components/PostList.vue';
     export default {
+        components: {
+            PostForm, PostList,
+        },
         data(){
             return{
                 posts: [
                     { id: 1, title: 'Пост о java script 1', body: 'java script универсальный ЯП',},
                     { id: 2, title: 'Пост о java script 2', body: 'java script универсальный ЯП',},
                     { id: 3, title: 'Пост о java script 3', body: 'java script универсальный ЯП',},
+                    { id: 4, title: 'Пост о java script 4', body: 'java script универсальный ЯП',},
                 ],
-                title: '',
-                body: '',
             }
         },
         methods: {
-            createPost(){
-                const newPost = {
-                    id: Date.now(),
-                    title: this.title,
-                    body: this.body,
-                }
-                this.posts.push(newPost);
-                this.title = '';
-                this.body = '';
+            createPost(post){
+                this.posts.push(post);
             },
+            removePost(post){
+                this.posts = this.posts.filter(p => p.id !== post.id)
+            }
         }
     }
 </script>
 
 <style>
-* { 
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-.app{
-    padding: 20px;
-}
-form{
-    display: flex;
-    flex-direction: column;
-}
-.post{
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-}
-.input{
-    width: 100%;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    margin-top: 10px;
-}
-.btn{
-    margin-top: 15px;
-    align-self: flex-end;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    background: none;
-    color: teal;
-    transition: 0.3s;
-}
-.btn:hover{
-    background: teal;
-    color: white;
-    border: 1px solid white;
-}
+    * { 
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .app{
+        padding: 20px;
+    }
 </style>
---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default{
+        name: 'my-button'
+    }
+</script>
+
+<style scoped>
+.btn{
+        border: 1px solid teal;
+        padding: 10px 15px;
+        background: none;
+        color: teal;
+        transition: 0.3s;
+    }
+    .btn:hover{
+        background: teal;
+        color: white;
+        border: 1px solid white;
+    }
+</style>
